06b124f325744261463f65a8d56f70059ac456ee
Opera.Wang@gmail.com <wangvisual@users.noreply.github.com>
2011-03-10 11:06:18 +0000

    ignore svn files

--- release.bat
+++ release.bat
@@ -1,11 +1,11 @@
 set PATH=c:\Program Files\7-Zip;%PATH%
-set zip=7z.exe a -tzip
+set zip=7z.exe a -tzip -r
 
 set ChromeFiles=content locale skin
 set AllFiles=chrome defaults modules chrome.manifest icon.png install.rdf
 
 del chrome\*.jar
 del gmailui-*-tb.xpi
-%zip% t.zip %ChromeFiles%
+%zip% t.zip %ChromeFiles% -x!.svn
 mv t.zip chrome/expressionsearch.jar
-%zip% gmailui-0.7-tb.xpi %AllFiles%
+%zip% gmailui-0.7-tb.xpi %AllFiles% -xr!.svn
