b53dca1cbbab78980c55688c2c2ae8a9a3483988
Opera.Wang@gmail.com <wangvisual@users.noreply.github.com>
2011-03-22 03:12:07 +0000

    no need to restart if change move2bar option

--- release.bat
+++ release.bat
@@ -1,4 +1,4 @@
 set zip="c:\Program Files\7-Zip\7z.exe" a -tzip -r
-set AllFiles=content local skin defaults modules chrome.manifest icon.png install.rdf
+set AllFiles=content locale skin defaults modules chrome.manifest icon.png install.rdf
 del gmailui-*-tb.xpi
-%zip% gmailui-0.8pre-tb.xpi %AllFiles% -xr!.svn
+%zip% gmailui-0.8beta-tb.xpi %AllFiles% -xr!.svn
