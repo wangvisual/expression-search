762d8c5d3f23c3c236b3342f49309654aa3b7709
Opera.Wang@gmail.com <wangvisual@users.noreply.github.com>
2013-05-20 14:39:42 +0000

    use PATH

--- release.bat
+++ release.bat
@@ -1,4 +1,5 @@
-set zip="c:\Program Files\7-Zip\7z.exe" a -tzip -mx1 -r
+set PATH=c:\Program Files (x86)\7-Zip;c:\Program Files\7-Zip;d:\Program Files (x86)\7-Zip;d:\Program Files\7-Zip
+set zip=7z.exe a -tzip -mx1 -r
 set AllFiles=content locale skin defaults modules chrome.manifest icon.png install.rdf
 del gmailui-*-tb.xpi
 %zip% gmailui-0.8.5-tb.xpi %AllFiles% -xr!.svn
