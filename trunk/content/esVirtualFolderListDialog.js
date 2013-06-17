85284542db281ad518c5d951ab4142ecfd909bcf
Opera.Wang@gmail.com <Opera.Wang@gmail.com@c44fc041-e70d-197c-439d-d7e1e201c1e1>
2013-06-17 11:54:08 +0000

    virtualFolderList

--- trunk/content/esVirtualFolderListDialog.js
+++ trunk/content/esVirtualFolderListDialog.js
@@ -1,10 +1,8 @@
 "use strict";
 
 const { classes: Cc, Constructor: CC, interfaces: Ci, utils: Cu, results: Cr, manager: Cm } = Components;
-//Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource:///modules/mailServices.js");
-//Cu.import("resource://app/modules/gloda/utils.js");
-//Cu.import("resource://gre/modules/FileUtils.jsm");
+Cu.import("resource:///modules/iteratorUtils.jsm");
 Cu.import("resource://expressionsearch/aop.js");
 Cu.import("resource://expressionsearch/log.js");
 
@@ -15,18 +13,12 @@ let esVirtualFolderSelector = {
       window.removeEventListener("load", esVirtualFolderSelector.Load, false);
       // How to deal with multi select and reverse?
       esVirtualFolderSelector.hookedFunctions.push( ExpressionSearchaop.around( {target: window, method: 'ReverseStateFromNode'}, function(invocation) {
+        let result = invocation.proceed(); // change folder's state first
         let typeSel = document.getElementById('esFolderType');
         let row = invocation.arguments[0];
-        ExpressionSearchLog.log('ReverseStateFromNode: ' + row + ":" + typeSel.value);
-        if ( !typeSel || typeSel.value == 0 || !gFolderPickerTree.view.isContainer(row) || gFolderPickerTree.view.isContainerEmpty(row) ) return invocation.proceed();
-        
         let folder = GetFolderResource(row).QueryInterface(Ci.nsIMsgFolder);
-        if ( !folder ) return false;
-        //ExpressionSearchLog.logObject(folder,'folder',0);
-        //ExpressionSearchLog.logObject(folder.rootFolder,'root',0);
-        let type = typeSel.value;
-        let result = invocation.proceed(); // change folder's state first
-        esVirtualFolderSelector.changeSubFolder(type, folder);
+        if ( !typeSel || typeSel.value == 0 || !gFolderPickerTree.view.isContainer(row) || gFolderPickerTree.view.isContainerEmpty(row) || !folder) return result;
+        esVirtualFolderSelector.changeSubFolder(typeSel.value, folder);
         return result;
       })[0] );
     } catch (err) {
@@ -36,7 +28,7 @@ let esVirtualFolderSelector = {
   
   changeSubFolder: function(type, folder) {
     try {
-      for each (let child in fixIterator(folder.subFolders, Ci.nsIMsgFolder)) {
+      for (let child in fixIterator(folder.subFolders, Ci.nsIMsgFolder)) {
         child.setInVFEditSearchScope( folder.inVFEditSearchScope, false /* subscope, not implemented */ );
         if ( type == 2 && child.hasSubFolders && child.numSubFolders > 0 ) {
           esVirtualFolderSelector.changeSubFolder(type, child);
@@ -48,16 +40,19 @@ let esVirtualFolderSelector = {
   },
   
   changeAllFolder: function(state) {
-    let allFolders = MailServices.accounts.allFolders;
-    for (let folder in fixIterator(allFolders, Ci.nsIMsgFolder)) // not including server itself
-      folder.setInVFEditSearchScope(state, false);
-    
-    if ( !state ) return;    
-    for (let i = 0; i < MailServices.accounts.length; i++) { // just for better looking
-      let account = accounts.queryElementAt(i, Ci.nsIMsgAccount);
-      account.incomingServer.rootFolder.setInVFEditSearchScope(state, false);
+    try {
+      // not work for TB 5 & not inclue root dir
+      //let allFolders = MailServices.accounts.allFolders;
+      //for (let folder in fixIterator(allFolders, Ci.nsIMsgFolder))
+      //  folder.setInVFEditSearchScope(state, false);
+      let accounts = MailServices.accounts.accounts;
+      for ( let account in fixIterator(accounts, Ci.nsIMsgAccount) ) {
+        account.incomingServer.rootFolder.setInVFEditSearchScope(state, false);
+        esVirtualFolderSelector.changeSubFolder(2, account.incomingServer.rootFolder);
+      }
+    } catch (err) {
+      ExpressionSearchLog.logException(err);
     }
-    ExpressionSearchLog.log('done',1);
   },
 
 };
