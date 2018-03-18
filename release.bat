69c36fddde6ebb8a78e3c09042e46085c8074998
Opera Wang <Opera.Wang@gmail.com>
2018-03-18 21:35:42 +0800

    1.3 release

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 set PATH=c:\Program Files (x86)\7-Zip;c:\Program Files\7-Zip;d:\Program Files (x86)\7-Zip;d:\Program Files\7-Zip
 set zip=7z.exe a -tzip -mx1 -r
-set AllFiles=content locale skin defaults modules chrome.manifest icon.png install.rdf
+set AllFiles=content locale skin modules chrome.manifest icon.png install.rdf
 del gmailui-*-tb.xpi
 %zip% gmailui-1.3-tb.xpi %AllFiles% -xr!.svn
