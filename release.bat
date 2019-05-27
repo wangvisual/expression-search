b1d65644aadc2b0b8732a53c0c97a819f150245a
Opera Wang <Opera.Wang@gmail.com>
2019-05-27 23:51:41 +0800

    add icons

--- release.bat
+++ release.bat
@@ -1,5 +1,5 @@
 set PATH=c:\Program Files (x86)\7-Zip;c:\Program Files\7-Zip;d:\Program Files (x86)\7-Zip;d:\Program Files\7-Zip
 set zip=7z.exe a -tzip -mx1 -r
-set AllFiles=content locale skin chrome.manifest icon.png install.rdf bootstrap.js
+set AllFiles=content locale skin chrome.manifest manifest.json icon.png install.rdf bootstrap.js
 del gmailui-*-tb.xpi
 %zip% gmailui-1.4-tb.xpi %AllFiles% -xr!.svn
